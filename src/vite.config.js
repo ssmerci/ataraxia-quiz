abc3826464bef2ac3a62c4e9041eb6cbd8f7c199
ssmerci <ssmercity@gmail.com>
2025-07-26 22:57:13 +0530

    Add files via upload

---
+++ src/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages';
+
+export default defineConfig({
+  plugins: [react(), ghPages()],
+  base: '/ataraxia-quiz/', // Replace with your repository name
+});
